618d6f4b767bfe1fdd27019dd68d94f8d9a9ec97
wenzhishen <wenzhishen@icloud.com>
2016-03-24 23:16:59 +0800

    Initial proj

---
+++ Point24/Point24/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Point24
+//
+//  Created by Vincent on 3/24/16.
+//  Copyright © 2016 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Point24/Point24/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Point24
+//
+//  Created by Vincent on 3/24/16.
+//  Copyright © 2016 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
